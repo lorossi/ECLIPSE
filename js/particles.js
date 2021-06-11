1c5a896b5d75b2432806476c89b279ad3228bcc9
lorossi <contact@lorenzoros.si>
2021-06-11 19:14:38 +0200

    first semi-working version

---
+++ js/particles.js
@@ -0,0 +1,76 @@
+class Particle {
+  constructor(x, y, theta, max_life, noise, colors = false) {
+    this._colors = colors;
+
+    this._x = x;
+    this._y = y;
+    this._theta = theta;
+    this._hue = theta * 180 / Math.PI;
+    this._max_life = max_life;
+    this._noise = noise;
+
+    this._min_life = 0.4;
+    this._max_alpha = 0.3;
+    this._min_saturation = 25;
+    this._max_angle = Math.PI * 2;
+
+    this._noise_scl = 0.0015;
+    this._time_scl = 0.2;
+    this._scl = 10;
+
+    // calculate starting angle
+    this._angle = Math.atan2(this._y, this._x);
+  }
+
+  move(percent) {
+    // calculate time coordinates
+    const time_theta = Math.PI * 2 * percent;
+    const tx = this._time_scl * (Math.cos(time_theta) + 1);
+    const ty = this._time_scl * (Math.sin(time_theta) + 1);
+
+    // generate random length
+    const n = (this._noise.noise4D(this._x * this._noise_scl, this._y * this._noise_scl, tx, ty) + 1) / 2;
+    this._length = ((1 - this._min_life) * (n * this._max_life) + this._min_life * this._max_life) / this._scl;
+
+
+    this._points = [];
+    this._points.push({ x: this._x, y: this._y });
+    // generate each point
+    for (let i = 1; i < this._length; i++) {
+      const prev = this._points[i - 1];
+      const n = this._noise.noise4D(prev.x * this._noise_scl, prev.y * this._noise_scl, tx, ty);
+      // rotate each point by a certain angle 
+      const theta = n * this._max_angle + this._angle;
+      // calculate xy coordinates
+      const nx = prev.x + Math.cos(theta) * this._scl;
+      const ny = prev.y + Math.sin(theta) * this._scl;
+
+      this._points.push({ x: nx, y: ny });
+    }
+  }
+
+  show(ctx) {
+    ctx.save();
+
+    for (let i = 0; i < this._points.length - 1; i++) {
+      const alpha = ease((1 - i / this._points.length)) * this._max_alpha;
+      const saturation = ease((1 - i / this._points.length)) * (100 - this._min_saturation) + this._min_saturation;
+      const p1 = this._points[i];
+      const p2 = this._points[i + 1];
+
+      if (this._colors) {
+        ctx.strokeStyle = `hsla(${this._hue}, ${saturation}%, 50%, ${alpha})`;
+      } else {
+        ctx.strokeStyle = `rgba(235, 235, 235, ${alpha})`;
+      }
+
+      ctx.strokeWidth = 1;
+      ctx.beginPath();
+      ctx.moveTo(p1.x, p1.y);
+      ctx.lineTo(p2.x, p2.y);
+      ctx.stroke();
+    }
+
+    ctx.restore();
+  }
+}
--- js/sketch.js
+++ js/sketch.js
@@ -1,13 +1,94 @@
 class Sketch extends Engine {
   preload() {
-
+    this._max_particles = 500;
+    this._r = this.width / 6;
+    this._max_life = this.width / 2 - this._r;
+    this._duration = 900;
+    this._recording = true;
   }
 
   setup() {
-
+    // setup capturer
+    this._capturer_started = false;
+    if (this._recording) {
+      this._capturer = new CCapture({ format: "png" });
+    }
+    // setup noise
+    this._noise = new SimplexNoise();
+    // create particles
+    this._particles = [];
+    // angle increments
+    const d_theta = Math.PI * 2 / this._max_particles;
+    for (let i = 0; i < this._max_particles; i++) {
+      // particle angular coordinate
+      const theta = Math.random() * Math.PI * 2;
+      // convert to x-y coordinates
+      const x = Math.cos(theta) * this._r;
+      const y = Math.sin(theta) * this._r;
+      // create particle
+      this._particles.push(new Particle(x, y, theta, this._max_life, this._noise))
+    }
   }
 
   draw() {
+    // start capturer
+    if (!this._capturer_started && this._recording) {
+      this._capturer_started = true;
+      this._capturer.start();
+      console.log("%c Recording started", "color: green; font-size: 2rem");
+    }
 
+    // time calculation
+    const percent = (this.frameCount % this._duration) / this._duration;
+
+    this.ctx.save();
+
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.ctx.fillStyle = "rgb(15, 15, 15)";
+    this.ctx.fillRect(0, 0, this.width, this.height);
+
+    this.ctx.translate(this.width / 2, this.height / 2);
+
+    // draw central ring
+    this.ctx.save();
+    this.ctx.strokeStyle = "rgb(235, 235, 235)";
+    this.ctx.lineWidth = 5;
+    this.ctx.beginPath();
+    this.ctx.arc(0, 0, this._r, 0, Math.PI * 2);
+    this.ctx.stroke();
+    this.ctx.restore();
+
+    // draw each particle
+    this._particles.forEach(p => {
+      p.move(percent);
+      p.show(this.ctx);
+    })
+
+    this.ctx.restore();
+
+    // handle recording
+    if (this._recording) {
+      if (this.frameCount % 60 == 0) {
+        const update = `Record: ${parseInt(percent * 100)}%`;
+        console.log(`%c ${update}`, "color: yellow; font-size: 0.75rem");
+      }
+      if (this.frameCount < this._duration) {
+        this._capturer.capture(this._canvas);
+      } else {
+        this._recording = false;
+        this._capturer.stop();
+        this._capturer.save();
+        console.log("%c Recording ended", "color: red; font-size: 2rem");
+      }
+    }
+
+    //this.noLoop();
+  }
+
+  mousedown() {
+    this.setup();
+    this.loop();
   }
 }
+
+const ease = (x) => 1 - (1 - x) * (1 - x);
